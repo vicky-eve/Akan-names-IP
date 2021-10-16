8c5084d6f5af53f38c694fe97b5a632c3baf65c5
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2021-10-16 20:58:55 +0300

    added functions in the javascript file that considers the gender and day of birth to output the respective Akan name

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('let us do this')
+> var day = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+var day = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+> var maleNames =  ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+> var femaleNames =  ["Akosua", "Adwoa", "Abenoa", "Akua", "Yaa", "Afua", "Ama"];
+> var yourName = [ maleNames[] || femaleNames[] ]
+> var name = function(gender, day) {
+};if (gender=Male && day[])
+  console.log("maleNames[]);
+} else if (gender=Female && day[])
+  console.log("femaleNames[]");
+} else {
+  console.log("No input");
+}
+alert(Your beautiful name is;)
+//get date input
+//get the gender input 
+//array of days
